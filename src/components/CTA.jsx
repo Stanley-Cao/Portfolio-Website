f1d950031a2098da0c0eef309ba9536e27f8c9a2
Stanley-Cao <stanleycao23@gmail.com>
2023-10-25 23:25:19 -0400

    Updated Responsiveness

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -4,7 +4,7 @@ const CTA = () => {
   return (
     <div className='cta'>
         <a href="https://drive.google.com/file/d/1gElGW-Ah3CyH5v0oCjcq3UCqb_-K6sk0/view?usp=sharing" className='btn' target="_blank" rel="noopener noreferrer">Resume</a>
-        <a href="#contact" className = 'btn btn-primary'>Let's Talk</a>
+        <a href="/Contact" className = 'btn btn-primary'>Let's Talk</a>
     </div>
   )
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../styles/contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
 
 const contact = () => {
   return (
@@ -15,12 +17,22 @@ const contact = () => {
             <h4>Email</h4>
             <h5>stanleycao24@gmail.com</h5>
             <a href="mailto:stanleycao24@gmail.com">Send a Message</a>
-            <h4>I am always looking to meet and talk with new people. <br></br>
-            Feel Free to drop a message and say hi.</h4>
+          </article>
+          <article className="contactOption">
+            <FaGithub className="mail"/>
+            <h4>GitHub</h4>
+            <h5>Stanley Cao</h5>
+            <a href="https://github.com/Stanley-Cao" target="_blank">My Profile</a>
+          </article>
+          <article className="contactOption">
+            <BsLinkedin className="mail"/>
+            <h4>LinkedIn</h4>
+            <h5>Stanley Cao</h5>
+            <a href="https://www.linkedin.com/in/stanley-cao/" target="_blank">Let's Connect</a>
           </article>
         </div>
         <form action="">
-          <h3>New Message</h3>
+          <h3>New Message <br></br> Feek free to say hi. I am always looking to meet new people or chat about opportunities.</h3>
           <input type="test" name='name' placeholder='Your Full Name' required/>
           <input type="email" name='email' placeholder='Your Email' required/>
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
