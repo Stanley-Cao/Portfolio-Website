0072617462886234836cd4a440247c04064f4bbd
Stanley-Cao <stanleycao23@gmail.com>
2023-10-31 13:53:19 -0400

    changes to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,22 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 
 
-const app = () => {
-  return (
-    <div>
-      <Router basename={"/"}>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact element={<About/>}/> 
-          <Route path="/Experience" exact element={<Experience/>}/>
-          <Route path="/Projects" exact element={<Projects/>}/>
-          <Route path="/Contact" exact element={<Contact/>}/>
-        </Routes>
-      </Router>
-    </div>
-  )
+class app extends React.Component {
+  render(){
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" exact element={<About/>}/> 
+            <Route path="/Experience" exact element={<Experience/>}/>
+            <Route path="/Projects" exact element={<Projects/>}/>
+            <Route path="/Contact" exact element={<Contact/>}/>
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default app
