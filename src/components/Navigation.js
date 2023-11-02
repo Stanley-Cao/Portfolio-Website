a2de28ff0d864d0b271ffce5ad99b39954fe6f2f
Stanley-Cao <stanleycao23@gmail.com>
2023-11-02 14:39:05 -0400

    updated nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import "../styles/navigation.css";
 import logo from '../resources/Jett.png';
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 const MobileNavbar = () => {
   const [openNavbar, setOpenNavbar] = useState(false);
@@ -15,10 +16,10 @@ const MobileNavbar = () => {
       <div className="navbar-links">
         <ul className="nav-items">
           <li className="nav-pages"><img className="jett" src={logo} style={{ width: 60, height: 55}} alt=""/></li>
-          <li className="nav-pages"><a href="/">HOME</a></li>
-          <li className="nav-pages"><a href="/#/Experience">EXPERIENCES</a></li>
-          <li className="nav-pages"><a href="/#/Projects">PROJECTS</a></li>
-          <li className="nav-pages"><a href="/#/Contact">CONTACT</a></li>
+          <li className="nav-pages"><Nav.Link href="/">HOME</Nav.Link></li>
+          <li className="nav-pages"><Nav.Link href="/#/Experience">EXPERIENCES</Nav.Link></li>
+          <li className="nav-pages"><Nav.Link href="/#/Projects">PROJECTS</Nav.Link></li>
+          <li className="nav-pages"><Nav.Link href="/#/Contact">CONTACT</Nav.Link></li>
         </ul>
       </div>
       <a href="#" className="toggle-button" onClick={() => setOpenNavbar(!openNavbar)}>
@@ -30,10 +31,10 @@ const MobileNavbar = () => {
       {windowWidth <= 1000 && openNavbar && (
         <div className="navbar-links-active">
           <ul className="nav-items">
-            <li className="nav-pages"><a href="/">HOME</a></li>
-            <li className="nav-pages"><a href="/#/Experience">EXPERIENCES</a></li>
-            <li className="nav-pages"><a href="/#/Projects">PROJECTS</a></li>
-            <li className="nav-pages"><a href="/#/Contact">CONTACT</a></li>
+            <li className="nav-pages"><Nav.Link href="/">HOME</Nav.Link></li>
+            <li className="nav-pages"><Nav.Link href="/#/Experience">EXPERIENCES</Nav.Link></li>
+            <li className="nav-pages"><Nav.Link href="/#/Projects">PROJECTS</Nav.Link></li>
+            <li className="nav-pages"><Nav.Link href="/#/Contact">CONTACT</Nav.Link></li>
           </ul>
         </div>
       )}
