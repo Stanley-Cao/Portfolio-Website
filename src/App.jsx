ffe82201ec24e3cb90a06533e3c91d3cda644ae1
Stanley-Cao <stanleycao23@gmail.com>
2023-11-02 14:27:49 -0400

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom';
+import { HashRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/Navigation';
 import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
-import {HashRouter} from 'react-router-dom';
 import Contact from './components/Contact';
 
 
-class App extends React.Component {
-  render(){
+function App() {
   return (
     <div>
-      <HashRouter>
         <Navbar />
         <Routes>
           <Route path="/" exact element={<About/>}/> 
@@ -20,10 +17,8 @@ class App extends React.Component {
           <Route path="/Projects" exact element={<Projects/>}/>
           <Route path="/Contact" exact element={<Contact/>}/>
         </Routes>
-      </HashRouter>
     </div>
   );
-}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import ReactDom from 'react-dom'
-import App from './App.jsx'
-import './styles/index.css'
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './App.jsx';
+import './styles/index.css';
+import { HashRouter } from 'react-router-dom';
 
-ReactDom.render(<App/>, document.querySelector("#root"))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+        <HashRouter baseline={process.env.PUBLIC_URL}>
+        <App />
+        </HashRouter>
+  </React.StrictMode>
+);
