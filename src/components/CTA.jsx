e4d135dced18d0cc8f45f2e516145e6aacdb485f
Stanley-Cao <stanleycao23@gmail.com>
2023-11-02 20:54:28 -0400

    updated navbar

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const CTA = () => {
   return (
     <div className='cta'>
         <a href="https://drive.google.com/file/d/1gElGW-Ah3CyH5v0oCjcq3UCqb_-K6sk0/view?usp=sharing" className='btn' target="_blank" rel="noopener noreferrer">Resume</a>
-        <a href="/Contact" className = 'btn btn-primary'>Let's Talk</a>
+        <Link to="/Contact" className = 'btn btn-primary'>Let's Talk</Link>
     </div>
   )
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import "../styles/navigation.css";
 import logo from '../resources/Jett.png';
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 
 const MobileNavbar = () => {
   const [openNavbar, setOpenNavbar] = useState(false);
   const [windowWidth, setWindowWidth] = useState(0);
+  const [url, setUrl] = useState("#")
 
   useEffect(() => {
     setWindowWidth(window.innerWidth);
@@ -16,25 +18,25 @@ const MobileNavbar = () => {
       <div className="navbar-links">
         <ul className="nav-items">
           <li className="nav-pages"><img className="jett" src={logo} style={{ width: 60, height: 55}} alt=""/></li>
-          <li className="nav-pages"><Nav.Link href="/">HOME</Nav.Link></li>
-          <li className="nav-pages"><Nav.Link href="/#/Experience">EXPERIENCES</Nav.Link></li>
-          <li className="nav-pages"><Nav.Link href="/#/Projects">PROJECTS</Nav.Link></li>
-          <li className="nav-pages"><Nav.Link href="/#/Contact">CONTACT</Nav.Link></li>
+          <li className="nav-pages"><Link to="/">HOME</Link></li>
+          <li className="nav-pages"><Link to="/Experience">EXPERIENCES</Link></li>
+          <li className="nav-pages"><Link to="/Projects">PROJECTS</Link></li>
+          <li className="nav-pages"><Link to="/Contact">CONTACT</Link></li>
         </ul>
       </div>
-      <a href="#" className="toggle-button" onClick={() => setOpenNavbar(!openNavbar)}>
+      <Link to={url} className="toggle-button" onClick={(event) => {event.preventDefault(); setOpenNavbar(!openNavbar);}}>
         <span className="toggle-bar"></span>
         <span className="toggle-bar"></span>
         <span className="toggle-bar"></span>
-      </a>
+      </Link>
       <div className="site-name">Stanley Cao </div>
       {windowWidth <= 1000 && openNavbar && (
         <div className="navbar-links-active">
           <ul className="nav-items">
-            <li className="nav-pages"><Nav.Link href="/">HOME</Nav.Link></li>
-            <li className="nav-pages"><Nav.Link href="/#/Experience">EXPERIENCES</Nav.Link></li>
-            <li className="nav-pages"><Nav.Link href="/#/Projects">PROJECTS</Nav.Link></li>
-            <li className="nav-pages"><Nav.Link href="/#/Contact">CONTACT</Nav.Link></li>
+            <li className="nav-pages" onClick={() => {setUrl("/")}}><Link to="/" onClick={() => {setOpenNavbar(!openNavbar)}}>HOME</Link></li>
+            <li className="nav-pages" onClick={() => {setUrl("/Experience")}}><Link to="/Experience" onClick={() => {setOpenNavbar(!openNavbar)}}>EXPERIENCES</Link></li>
+            <li className="nav-pages" onClick={() => {setUrl("/Projects")}}><Link to="/Projects" onClick={() => {setOpenNavbar(!openNavbar)}}>PROJECTS</Link></li>
+            <li className="nav-pages" onClick={() => {setUrl("/Contact")}}><Link to="/Contact" onClick={() => {setOpenNavbar(!openNavbar)}}>CONTACT</Link></li>
           </ul>
         </div>
       )}
